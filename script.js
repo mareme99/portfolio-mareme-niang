7fea1a534912da7e22788f262555caccead8354b
mareme99 <niangmareme349@gmail.com>
2025-05-31 22:43:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+// Défilement fluide pour les liens de navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
